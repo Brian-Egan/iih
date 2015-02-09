bb8edc1d51c4f97cf31093e85975b108ff69c64d
Brian Egan <BrianEgan@outlook.com>
2015-02-09 10:53:57 -0500

    Self

--- lib/iih/iih.rb
+++ lib/iih/iih.rb
@@ -1,12 +1,16 @@
 module Iih
 
 
-    def pt(*args)
-      return (args.length == 1) ? PM.logger.debug(args) : ap(args)
-      # return false
-      # ap args
-    end
+    class << self
 
+
+        def pt(*args)
+          return (args.length == 1) ? PM.logger.debug(args) : ap(args)
+          # return false
+          # ap args
+        end
+
+    end
 
 
 end
